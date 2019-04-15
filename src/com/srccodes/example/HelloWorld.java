f0335d075a6e89f8b9ac3d3f8740137123dafeb9
Dev Vivavox <Dev Vivavox@DESKTOP-VR761QE>
2019-04-15 11:49:16 -0300

    Commit inicial Hello World

---
+++ src/com/srccodes/example/HelloWorld.java
@@ -0,0 +1,56 @@
+package com.srccodes.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+        PrintWriter out  = response.getWriter();
+        out.println("<HMTL>");
+        out.println("<HEAD>");
+        out.println("<TITLE>Hello World Servlets</TITLE>");
+        out.println("</HEAD>");
+        out.println("<BODY BGCOLOR=\"white\">");
+        out.println("<P>Hello World</P>");
+        out.println("</BODY>");
+        out.println("</HTML>");
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	
+
+}
